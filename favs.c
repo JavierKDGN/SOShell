ceab659e26c6bba1f1b5b894c2bd0ca183c85f65
JavierKDGN <javier.cadagan6@gmail.com>
2024-09-07 02:20:58 -0400

    Arreglos en argumentos y punteros de funciones en fav, arreglo en bugs de logica en for y en ifs

--- favs.c
+++ favs.c
@@ -22,11 +22,11 @@ typedef enum {
 //Struct que define un comando preferido
 typedef struct {
     char comando[MAX_COMANDOS];
-    size_t id;
+    int id;
     bool eliminado;
 } ComFavorito;
 
-bool estaEnFavs(ComFavorito *favs, size_t *num_favs, const char *comando) {
+bool estaEnFavs(ComFavorito *favs, int *num_favs, const char *comando) {
     for (int i = 0; i < *num_favs; i++) {
         if (strcmp(favs[i].comando, comando) == 0) {
             if (favs[i].eliminado == true) return false;
@@ -36,13 +36,13 @@ bool estaEnFavs(ComFavorito *favs, size_t *num_favs, const char *comando) {
     return false;
 }
 
-void addFav(ComFavorito *favs, size_t *num_favs, const char *comando) {
+void addFav(ComFavorito *favs, int *num_favs, const char *comando) {
     if (*num_favs >= MAX_FAVS) {
-        fprintf(stderr, "Error: lista de favoritos llena (%d de %d).\n", num_favs, MAX_FAVS);
+        fprintf(stderr, "Error: lista de favoritos llena (%d de %d).\n", *num_favs, MAX_FAVS);
         return;
     }
 
-    if (!estaEnFavs(favs, *num_favs, comando)) { //FALTA IMPLEMENTAR
+    if (!estaEnFavs(favs, num_favs, comando)) { //FALTA IMPLEMENTAR
         favs[*num_favs].id = *num_favs + 1; //agrega a favs el id nuevo
         strncpy(favs[*num_favs].comando, comando, MAX_COMANDOS - 1); //asigna a ese id el comando ingresado
         favs[*num_favs].comando[MAX_COMANDOS - 1] = '\0'; // pone el caracter para terminar la string
@@ -51,22 +51,25 @@ void addFav(ComFavorito *favs, size_t *num_favs, const char *comando) {
     }
 }
 
-void mostrarFavs(ComFavorito *favs, size_t *num_favs) {
+void mostrarFavs(ComFavorito *favs, int *num_favs) {
     for (int i = 0; i < *num_favs; i++) {
-        if (favs[i].eliminado == true) continue;
-        printf("%d: %s\n", favs[i].id, favs[i].comando);
+        if (favs[i].eliminado == true) {
+            continue;
+        } else {
+            printf("%d: %s\n", favs[i].id, favs[i].comando);
+        }
     }
 }
 
 void eliminarParFavs(ComFavorito *favs, int id1, int id2) {
     int dif_abs = (id1 - id2) * (((id1 - id2) > 0) - (id1 - id2) < 0); //algoritmo para sacar el valor absoluto
     int menor;
-    if (id1 > id2) {
+    if (id1 < id2) {
         menor = id1;
     } else menor = id2;
 
     int i = 0;
-    for (i = menor; i <= menor + dif_abs; i++) {
+    for (i = menor - 1; i <= menor + dif_abs; i++) {
         favs[i].eliminado = true;
     }
 
@@ -79,8 +82,46 @@ void borrarFavs(ComFavorito **favs, int *num_favs) {
         return;
     }
     free(*favs);
-    favs = new_favs;
+    *favs = new_favs;
     *num_favs = 0;
 }
 
 
+/* TEST GENERADO POR COPILOT, EN LA COMMIT ACTUAL FUNCIONA CORRECTAMENTE
+int main() {
+    // Allocate memory for favorites
+    ComFavorito *favs = malloc(sizeof(ComFavorito) * MAX_FAVS);
+    if (favs == NULL) {
+        fprintf(stderr, "Error: No se pudo asignar memoria para los favoritos.\n");
+        return 1;
+    }
+    int num_favs = 0;
+
+    // Add some favorites
+    addFav(favs, &num_favs, "comando1");
+    addFav(favs, &num_favs, "comando2");
+    addFav(favs, &num_favs, "comando3");
+
+    // Show favorites
+    printf("Lista de favoritos:\n");
+    mostrarFavs(favs, &num_favs);
+
+    // Check if a command is in favorites
+    const char *comando = "comando2";
+    if (estaEnFavs(favs, &num_favs, comando)) {
+        printf("El comando '%s' está en la lista de favoritos.\n", comando);
+    } else {
+        printf("El comando '%s' no está en la lista de favoritos.\n", comando);
+    }
+
+    // Clear favorites
+    eliminarParFavs(favs,2,3);
+    mostrarFavs(favs,&num_favs);
+
+    // Clean up
+    free(favs);
+
+    return 0;
+}
+
+*/
